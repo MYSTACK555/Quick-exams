e071f0cf7f0ae5f5d8e48174c3f97b3680cd9180
Michael Labrecque <micklabrecque@hotmail.com>
2021-02-25 20:49:11 -0500

    Revert "test to rewind after"
    This reverts commit d9a0fb87627d5a3ce60b983b708c8c907031271c.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,10 @@
 module.exports = function(config){
 
     config.addPassthroughCopy("src/js");
-    config.addPassthroughCopy("src/css");
-    config.addPassthroughCopy("node_modules/@fortawesome/fontawesome-free");
     return{
         dir:{
             input: "src",
-            output: "dist",
-            data: "_data"
+            output: "dist"
         }
     };
 };
