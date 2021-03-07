81dc204021fc0bc9be80310c183085d8b5614647
Michael Labrecque <micklabrecque@hotmail.com>
2021-03-06 19:59:43 -0500

    oubli

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,12 @@ module.exports = function(config){
 
     config.addPassthroughCopy("src/js");
     config.addPassthroughCopy("src/asset");
+    config.addPassthroughCopy("src/css");
     return{
         dir:{
             input: "src",
-            output: "dist"
+            output: "dist",
+            data: "_data"
         }
     };
 };
