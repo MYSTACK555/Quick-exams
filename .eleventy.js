13374e25f61338028aea796b312948e1c6f3b495
Michael Labrecque <micklabrecque@hotmail.com>
2021-03-10 11:21:17 -0500

    retirer netlify-cms

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function(config){
 
     config.addPassthroughCopy("src/js");
     config.addPassthroughCopy("src/asset");
-    config.addPassthroughCopy("src/admin");
     config.addPassthroughCopy("src/css");
     return{
         dir:{
