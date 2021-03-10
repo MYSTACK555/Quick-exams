6169eeb849728c86dfaa52f601bba45585ddad62
Michael Labrecque <micklabrecque@hotmail.com>
2021-03-10 10:42:15 -0500

    xcz

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(config){
 
     config.addPassthroughCopy("src/js");
     config.addPassthroughCopy("src/asset");
+    config.addPassthroughCopy("src/admin");
     config.addPassthroughCopy("src/css");
     return{
         dir:{
