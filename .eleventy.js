3535e346d45da4d8b91104b2d9fa322af1cb200b
Michael Labrecque <micklabrecque@hotmail.com>
2021-03-27 19:32:59 -0400

    effectivly get product from stripe

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function(config){
     config.addPassthroughCopy("src/js");
     config.addPassthroughCopy("src/asset");
     config.addPassthroughCopy("src/css");
+    config.addPassthroughCopy({"node_modules/stripe/lib/":"js/stripe/"});
+    
     return{
         dir:{
             input: "src",
---
+++ src/_data/marketAPI.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+function joinPrice(product,listPrices) {
+    try {
+        let priceItem=listPrices.filter(x=>{return x.product===product.id});
+        return {
+			"code": product.id,
+            "name": product.name,
+            "description": product.description,
+			"prix": priceItem[0].unit_amount/100,
+			"devise": priceItem[0].currency,
+			"frequence": priceItem[0].type,
+		};
+	} catch (error) {
+        console.log(error);
+	}
+}
+
+module.exports = async function () {
+    const products = await stripe.products.list({});
+    const prices = await stripe.prices.list({});
+	//return formatProducts(products);
+    var promises = products.data.map(x=>joinPrice(x,prices.data))
+    return Promise.all(promises).then((nobbject) => {
+		console.log("nobject:", nobbject);
+		return [].concat.apply([], nobbject);
+	});
+};
