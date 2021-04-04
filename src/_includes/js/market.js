b8cd2f449983ca97d4e1a45b8fef34a7baea599a
Michael Labrecque <micklabrecque@hotmail.com>
2021-04-04 00:14:15 -0400

    test

--- src/_includes/js/market.js
+++ src/_includes/js/market.js
@@ -4,14 +4,30 @@ for (var item of document.getElementsByName("buyingForm")) {
 	item.addEventListener("submit", handler); 
 } 
  
-function handler(event) 
-{ 
-	event.preventDefault; 
-	const form = new FormData(event.target); 
- 
-	const data = { 
-		sku: form.get("sku"), 
-		quantity: Number(1),//can only buy one virtual product 
-	}; 
-
+function handler(event)  
+{  
+	event.preventDefault;  
+	const form = new FormData(event.target);  
+  
+	const data = {  
+		sku: form.get("sku"),  
+		quantity: Number(1),//can only buy one virtual product  
+	};  
+	alert(data.sku);
+	const response = await fetch('/.netlify/functions/create-checkout', {
+		method: 'POST',
+		headers: {
+		  'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	  }).then((res) => res.json());
+	
+	  const stripe = Stripe(response.publishableKey);
+	  const { error } = await stripe.redirectToCheckout({
+		sessionId: response.sessionId,
+	  });
+	
+	  if (error) {
+		console.error(error);
+	  }
 }
